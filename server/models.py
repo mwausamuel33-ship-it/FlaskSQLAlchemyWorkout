fa0f075302e2524593ddf7896fc3ac2481468cb8
Developer <test@example.com>
2026-04-15 13:39:06 +0300

    lab summative submission

--- server/models.py
+++ server/models.py
@@ -13,7 +13,7 @@ class Exercise(db.Model):
     __tablename__ = 'exercises'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False, unique=True)
+    name = db.Column(db.String(255), nullable=False)
     category = db.Column(db.String(100), nullable=False)
     equipment_needed = db.Column(db.Boolean, default=False)
 
